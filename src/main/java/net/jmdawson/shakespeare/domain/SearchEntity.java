f76521757839d25e25b74fed8b1fc1f3e08297f2
Jacob M. Dawson <dawson@vt.edu>
2014-10-30 18:34:55 -0400

    Fixed up relationship with other entities

--- src/main/java/net/jmdawson/shakespeare/domain/SearchEntity.java
+++ src/main/java/net/jmdawson/shakespeare/domain/SearchEntity.java
@@ -1,15 +1,21 @@
 package net.jmdawson.shakespeare.domain;
 
 import java.util.Date;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import net.jmdawson.shakespeare.PodReport;
 import net.jmdawson.shakespeare.Search;
+import net.jmdawson.shakespeare.Segment;
 import net.jmdawson.shakespeare.ShakespeareUser;
 
 /**
@@ -36,10 +42,16 @@ public class SearchEntity extends AbstractEntity implements Search {
   private String searchNumber;
   
   @Column (name = "search")
-  private ShakespeareUser createdBy;
+  private ShakespeareUserEntity createdBy;
   
   @Column (name = "created_date")
   private Date createdDate;
+  
+  @OneToMany(fetch = FetchType.LAZY, mappedBy = "search_id")
+  private Set<PodReportEntity> podReports = new LinkedHashSet<>(); 
+  
+  @OneToMany(fetch = FetchType.LAZY, mappedBy = "search_id")
+  private Set<SegmentEntity> segments = new LinkedHashSet<>();
 
   @Override
   public int getId() {
@@ -73,7 +85,7 @@ public class SearchEntity extends AbstractEntity implements Search {
     return createdBy;
   }
 
-  public void setCreatedBy(ShakespeareUser createdBy) {
+  public void setCreatedBy(ShakespeareUserEntity createdBy) {
     this.createdBy = createdBy;
   }
 
@@ -84,6 +96,22 @@ public class SearchEntity extends AbstractEntity implements Search {
 
   public void setCreatedDate(Date createdDate) {
     this.createdDate = createdDate;
+  }
+  
+  public Set<? extends PodReport> getPodReports(){
+    return podReports;
+  }
+  
+  public void setPodReports(Set<PodReportEntity> podReports){
+    this.podReports = podReports;
+  }
+  
+  public Set<? extends Segment> getSegments(){
+    return segments;
+  }
+  
+  public void setSegments(Set<SegmentEntity> segments){
+    this.segments = segments;
   }
 
 }
