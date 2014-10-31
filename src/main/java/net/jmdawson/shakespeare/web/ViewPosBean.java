d4b58dc45828304ec5fbb22e2d48da6debdf1c95
Jacob M. Dawson <dawson@vt.edu>
2014-10-31 01:04:27 -0400

    Changed to RequestScoped for now
    Need to determine if this is the right idea.  Not sure, as it's a form
    as well as a list view. I'm leaning back towards convo scoped, but I
    need to fix the serialization issue first.

--- src/main/java/net/jmdawson/shakespeare/web/ViewPosBean.java
+++ src/main/java/net/jmdawson/shakespeare/web/ViewPosBean.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.Conversation;
-import javax.enterprise.context.ConversationScoped;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -13,28 +13,25 @@ import net.jmdawson.shakespeare.service.ViewSegmentModel;
 import net.jmdawson.shakespeare.service.ViewSegmentsService;
 
 @Named
-@ConversationScoped
-public class ViewPosBean implements Serializable{
+@RequestScoped
+public class ViewPosBean implements Serializable {
 
   private static final long serialVersionUID = -4693914242994888744L;
-  
-  @Inject
-  protected Conversation conversation;
+
+//  @Inject
+//  protected Conversation conversation;
 
   @Inject
   protected ViewSegmentsService service;
-  
-  
+
   private List<ViewSegmentModel> segments;
-  
+
   @PostConstruct
-  public void init(){
-    if(conversation.isTransient()) conversation.begin();
-    segments = service.getSegments();
+  public void init() {
+      segments = service.getSegments();
   }
-  
-  public List<ViewSegmentModel> getSegments(){
-    
+
+  public List<ViewSegmentModel> getSegments() {
     return segments;
   }
 
