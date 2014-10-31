eb81be3b52041cdf118d6cb68e0bc5636a772217
Jacob M. Dawson <dawson@vt.edu>
2014-10-30 20:11:44 -0400

    Added Javadocs and a method for updating a segment

--- src/main/java/net/jmdawson/shakespeare/service/ViewSegmentsService.java
+++ src/main/java/net/jmdawson/shakespeare/service/ViewSegmentsService.java
@@ -2,8 +2,23 @@ package net.jmdawson.shakespeare.service;
 
 import java.util.List;
 
+/**
+ * A service to view segments and edit their pods
+ *
+ * @author Jacob M. Dawson
+ */
 public interface ViewSegmentsService {
 
+  /**
+   * Returns the list of all segments for the search
+   * @return
+   */
   List<ViewSegmentModel> getSegments();
+  
+  /**
+   * Update a segment based on a model
+   * @param model
+   */
+  void updateSegment(ViewSegmentModel model);
 
 }
