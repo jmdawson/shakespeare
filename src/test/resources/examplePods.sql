770d2b854ad7f4d2207ea4d76b24f2720048280c
Jacob M. Dawson <dawson@vt.edu>
2014-10-31 00:14:09 -0400

    Added some users and searches to flesh out the test environment

--- src/test/resources/examplePods.sql
+++ src/test/resources/examplePods.sql
@@ -1,2 +1,4 @@
+insert into user (id,common_name,given_name,surname,created_date) values ('234','Jacob M. Dawson','Jacob','Dawson','2014-10-27'),('1123','Tracy McElroy','Tracy','McElroy','2014-10-27');
+insert into search (id,initiation_date, va_search_number,created_by,created_date) values ('234','2014-10-27','VA-000-14','1123','2014-10-27');
 insert into segment(id, segment_number, search_id, created_by, created_date, last_updated_by, last_updated_date) values(2,1,234,1123,'2014-10-27',234,'2014-10-27');
 insert into segment(id, segment_number, search_id, created_by, created_date, last_updated_by, last_updated_date) values(1,1,234,1123,'2014-10-27',234,'2014-10-27');
