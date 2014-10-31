b9f6b55b6080d15990096183f796e3595e933989
Jacob M. Dawson <dawson@vt.edu>
2014-10-30 21:25:06 -0400

    Fleshed out ViewSegmentsServiceBean

--- src/main/java/net/jmdawson/shakespeare/service/ViewSegmentsServiceBean.java
+++ src/main/java/net/jmdawson/shakespeare/service/ViewSegmentsServiceBean.java
@@ -5,18 +5,27 @@ import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 import net.jmdawson.shakespeare.Segment;
+import net.jmdawson.shakespeare.repository.PodReportRepository;
 import net.jmdawson.shakespeare.repository.SegmentRepository;
 
 @Transactional
 @ApplicationScoped
 public class ViewSegmentsServiceBean implements ViewSegmentsService {
+
+  @Inject
+  protected SegmentRepository segmentRepository;
   
   @Inject
-  protected SegmentRepository repository;
+  protected PodReportRepository podReportRepository;
   
+  @PersistenceContext
+  protected EntityManager entityManager;
+
   @Inject
   protected ViewSegmentModelFactory factory;
 
@@ -25,13 +34,20 @@ public class ViewSegmentsServiceBean implements ViewSegmentsService {
    */
   @Override
   public List<ViewSegmentModel> getSegments() {
-    List<Segment> segments = repository.getSegments();
+    List<Segment> segments = segmentRepository.getSegments();
     List<ViewSegmentModel> models = new ArrayList<>();
-    
-    for (Segment segment : segments){
+
+    for (Segment segment : segments) {
       models.add(factory.createModel(segment));
     }
-   return models;
+    return models;
+  }
+
+  @Override
+  public void updateSegment(ViewSegmentModel model)
+      throws SegmentRepositoryException {
+    ConcreteViewSegmentModel concrete = (ConcreteViewSegmentModel) model;
+    entityManager.merge(concrete.getEntity());
   }
 
 }
